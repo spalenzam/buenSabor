042d5bc026137a1bb6ab1e2b20b522d9979cd15d
MarPotichkin <62658095+MarPotichkin@users.noreply.github.com>
2021-06-15 10:28:35 -0300

    Add files via upload

---
+++ BuenSabor/src/main/java/com/buenSabor/services/ConfiguracionService.java
@@ -0,0 +1,8 @@
+package com.buenSabor.services;
+
+import com.buenSabor.entity.Configuracion;
+import com.commons.services.CommonService;
+
+public interface ConfiguracionService extends CommonService<Configuracion>{
+
+}
