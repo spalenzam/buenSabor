1ae4b259275ab3cb9ad83d717ac4d2be1c068b97
MarPotichkin <62658095+MarPotichkin@users.noreply.github.com>
2021-06-15 10:27:11 -0300

    Add files via upload

---
+++ BuenSabor/src/main/java/com/buenSabor/entity/Configuracion.java
@@ -0,0 +1,43 @@
+package com.buenSabor.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "configuracion")
+public class Configuracion {
+	
+	@Column(name = "cantidad_cocineros")
+	private int cantidadCocineros;
+	
+	@Column(name = "email_empresa")
+	private String emailEmpresa;
+	
+	@Column(name = "token_mercado_pago")
+	private String tokenMercadoPago;
+
+	public int getCantidadCocineros() {
+		return cantidadCocineros;
+	}
+
+	public void setCantidadCocineros(int cantidadCocineros) {
+		this.cantidadCocineros = cantidadCocineros;
+	}
+
+	public String getEmailEmpresa() {
+		return emailEmpresa;
+	}
+
+	public void setEmailEmpresa(String emailEmpresa) {
+		this.emailEmpresa = emailEmpresa;
+	}
+
+	public String getTokenMercadoPago() {
+		return tokenMercadoPago;
+	}
+
+	public void setTokenMercadoPago(String tokenMercadoPago) {
+		this.tokenMercadoPago = tokenMercadoPago;
+	}
+}
