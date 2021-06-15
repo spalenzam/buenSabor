53ed03cb0c4beaffbf3c86058ad6821e18352367
MarPotichkin <62658095+MarPotichkin@users.noreply.github.com>
2021-06-15 10:27:46 -0300

    Add files via upload

---
+++ BuenSabor/src/main/java/com/buenSabor/repository/ConfiguracionRepository.java
@@ -0,0 +1,9 @@
+package com.buenSabor.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.buenSabor.entity.Configuracion;
+
+public interface ConfiguracionRepository extends CrudRepository<Configuracion,Long> {
+
+}
